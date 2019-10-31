e7f857e2a58630e09587d0470d23002c9c3b7e02
Caleb Thompson <caleb.thompson@xtra.co.nz>
2019-10-31 16:51:53 +1300

    record button now interactable

--- client/components/Controls.jsx
+++ client/components/Controls.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 
 class Controls extends Component {
+  state = {
+    recording: false
+  }
+
+  toggleRecord = () => {
+    this.setState({
+      recording: !this.state.recording
+    })
+  }
+
   render() {
+    console.log(this.state.recording)
     return (
       <div id="controls">
         <button id="clear" className="round control">Clear Recording</button>
-        <button id="record" className="round control">
-          <span>
+        <button id="record" className="round control" onClick={this.toggleRecord}>
+          <span id={this.state.recording ? "rec-status": ""}>
             <div id="inner-record">
               <p>Record</p>
             </div>
