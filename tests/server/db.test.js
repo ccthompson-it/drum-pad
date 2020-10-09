776ce9eaf8e98b98ddb5afe725125e5f4c27a420
Caleb Thompson <caleb.thompson@xtra.co.nz>
2020-10-09 14:33:36 +1300

    tested db functions

--- tests/server/db.test.js
+++ tests/server/db.test.js
@@ -18,4 +18,25 @@ describe('Beat db', () => {
         expect(beats.length).toBe(2)
       })
   })
+
+  test('deleteBeat deletes the specific beat', async () => {
+    expect.assertions(2)
+    await db.deleteBeat(1, testDb)
+    return db.getBeats(testDb)
+      .then(beats => {
+        expect(beats.length).toBe(1)
+        expect(beats[0].id).toBe(2)
+      })
+  })
+
+  test('saveBeat adds a new beat to the db', async () => {
+    expect.assertions(2)
+    const fakeBeat = {beat_name: 'Amazing Beat', beat: '[{"sound":"basskick","timing":1595732636364},{"sound":"clap","timing":1595732636792}]'}
+    await db.saveBeat(fakeBeat, testDb)
+    return db.getBeats(testDb)
+      .then(beats => {
+        expect(beats.length).toBe(3)
+        expect(beats[2].beat_name).toBe('Amazing Beat')
+      })
+  })
 })
